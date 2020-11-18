4dd996c102c4ba1c0937868535c900dc8354bac4
Saurabh <saurabh.nit20@gmail.com>
2020-11-18 14:30:17 +0530

    [Saurabh].Regex Validation

---
+++ Validation.js
@@ -0,0 +1,15 @@
+﻿//RegexJS Demo
+
+function CheckNameUsingRegex(name) {
+    let nameRegex = new RegExp(/^[A-Z][a-z]{2,}$/);
+
+    if (nameRegex.test(name)) {
+        console.log("Valid Name");
+    }
+    else {
+        console.log("Invalid Name");
+    }
+}
+
+CheckNameUsingRegex("Indal");
+CheckNameUsingRegex("indal");
