62755f9f00e05676d44b3138097d527a2410ba99
Saurabh <saurabh.nit20@gmail.com>
2020-11-18 13:33:34 +0530

    [Saurabh].Slicing Method

--- Add.js
+++ Add.js
@@ -3,6 +3,10 @@ let b = 20;
 let c = a + b;
 console.log("Sum is" + c);
 
+let s1 = "10";
+let s2 = "20";
+let s3 = s1+s2;
+
 let d = new Date();
 let today = "Today is" + (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear();
     let time = "Time" + d.getHours() + ":" + d.getMinutes();
